2aff9d988de4f565b48eaaa4d9c6b0ba017e9da8
Dudu <duducardoso2701@gmail.com>
2024-12-21 00:46:12 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: "app.component.html" ,
+  styles: [],
+})
+export class AppComponent {
+  title = 'portifolio';
+}
