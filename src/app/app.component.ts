9e475b9c5b6d7c948dccbc94fafc132f8e19fc1d
Dudu <duducardoso2701@gmail.com>
2024-12-21 01:41:27 -0300

    navbar pronta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: 'app.component.html',
   styleUrl: 'app.component.scss',
 })
 
-export class AppComponent {}
+export class AppComponent {
+  public colapsarNavBar: boolean = false;
+}
