6928e00a1db14859b8be3d2468223a4fc0bdf327
Dudu <duducardoso2701@gmail.com>
2025-01-04 13:52:50 -0300

    Habilidades finalizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgFor],
   templateUrl: 'app.component.html',
   styleUrl: 'app.component.scss',
 })
